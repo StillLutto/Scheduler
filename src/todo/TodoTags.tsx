17a72923ec9a515145d2bab2af4d826a3cf98e3c
StillLutto <iameinsteinhe@gmail.com>
2024-02-16 18:23:17 +0100

    feat(tags): add button to add new tags

--- src/todo/TodoTags.tsx
+++ src/todo/TodoTags.tsx
@@ -3,11 +3,14 @@ import "./TodoTags.css";
 function TodoTags() {
     return (
         <div className="todo__tags-container">
-            <h1 className="todo__tags-title">To-Do Tags</h1>
+            <div className="todo__tags-header">
+                <h1 className="todo__tags-header-title">To-Do Tags</h1>
+                <button className="todo__tags-header-button"><i className="fa-solid fa-plus button-icon"></i></button>
+            </div>
             <ul className="todo__tags">
                 <li>hi</li>
                 <li>hi</li>
-                    <li>hi</li>
+                <li>hi</li>
             </ul>
         </div>
     );
