24ffc591b2918283d49123ffb88b2990d03177fc
StillLutto <iameinsteinhe@gmail.com>
2024-02-16 18:25:47 +0100

    chore: fix wrong indentation

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,16 +3,16 @@ import "./Header.css";
 function Header() {
      return (
           <header>
-          <div className="header section-container">
-               <div className="header__title">
-                    <h1 className="header__title-text"><em>Scheduler</em></h1>
+               <div className="header section-container">
+                    <div className="header__title">
+                         <h1 className="header__title-text"><em>Scheduler</em></h1>
+                    </div>
+                    <div className="header__buttons">
+                         <i className="fa-regular fa-envelope fa-3x"></i>
+                         <i className="fa-solid fa-gear fa-3x"></i>
+                    </div>
                </div>
-               <div className="header__buttons">
-                    <i className="fa-regular fa-envelope fa-3x"></i>
-                    <i className="fa-solid fa-gear fa-3x"></i>
-               </div>
-          </div>
-     </header>
+          </header>
      );
 }
 
