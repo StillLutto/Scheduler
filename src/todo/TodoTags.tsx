30927040bb3f99bf73933b5288921c6d6285330f
StillLutto <iameinsteinhe@gmail.com>
2024-02-24 13:23:19 +0100

    fix: make plus button text actually centered

--- src/todo/TodoTags.tsx
+++ src/todo/TodoTags.tsx
@@ -6,7 +6,7 @@ function TodoTags() {
             <div className="todo__tags-header">
                 <h1 className="todo__tags-header-title">To-Do Tags</h1>
                 <div className="title-button-container">
-                <a href="/" className="title-button"><i className="fa-solid fa-plus"></i></a>
+                    <a href="/" className="title-button"><i className="fa-solid fa-plus"></i></a>
                 </div>
             </div>
             <ul className="todo__tags">
